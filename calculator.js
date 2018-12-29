bb91823fc5a9db8313cfe44d0fb4d6f58e6692ab
Robin Unger <robin.e.unger@gmail.com>
2018-12-28 18:11:16 -0600

    created function to update display

--- calculator.js
+++ calculator.js
@@ -17,3 +17,21 @@ function divide(a, b) {
 function operate(operator, a, b) {
     return operator(a, b);
 }
+
+function calculator() {
+    updateDisplay(0);
+    let displayValue = [];
+
+    const btns = document.querySelectorAll('.btn');
+    btns.forEach(btn => btn.addEventListener('click', function(e){
+        displayValue.push(this.textContent);
+        updateDisplay(displayValue);
+    }));
+}
+
+function updateDisplay(content = 0) {
+    if(content) content = content.join('');
+    document.getElementById('display').textContent = content;
+}
+
+calculator();
