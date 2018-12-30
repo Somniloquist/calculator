6c12290bd33737ffb45a5878185615234099ddf1
Robin Unger <robin.e.unger@gmail.com>
2018-12-29 18:57:01 -0600

    added operator buttons

--- calculator.js
+++ calculator.js
@@ -18,20 +18,59 @@ function operate(operator, a, b) {
     return operator(a, b);
 }
 
-function calculator() {
-    updateDisplay(0);
-    let displayValue = [];
 
-    const btns = document.querySelectorAll('.btn');
+function calculator() {
+    updateMainDisplay(0);
+    let displayValue = [];
+    let equation = [];
+
+    const btns = document.querySelectorAll('.btn-number');
+    const operators = document.querySelectorAll('.btn-operator');
+    const clear = document.getElementById('btn-clear');
+    const equals = document.getElementById('btn-equals');
+
     btns.forEach(btn => btn.addEventListener('click', function(e){
         displayValue.push(this.textContent);
-        updateDisplay(displayValue);
+        updateMainDisplay(displayValue);
     }));
+
+    operators.forEach(btn => btn.addEventListener('click', function(e) {
+        equation.push(displayValue.join(''));
+        displayValue = [];
+        if(equation.slice(-1).toString().match(/[\+\-\/\*]/g)) {
+            equation.pop();
+            equation.push(this.textContent);
+        } else {
+            equation.push(this.textContent);
+        }
+        updateMainDisplay();
+        updateSecondaryDisplay(equation);
+    }));
+
+    clear.addEventListener('click', function(e){
+        updateMainDisplay(0);
+        updateSecondaryDisplay();
+        displayValue = [];
+        equation = [];
+    });
+
+    // equals.addEventListener('click', function(e) {
+    //     equation.push(displayValue.join(''));
+    //     displayValue = [];
+    //     updateSecondaryDisplay();
+    // });
+
+
 }
 
-function updateDisplay(content = 0) {
+function updateSecondaryDisplay(content = []) {
     if(content) content = content.join('');
-    document.getElementById('display').textContent = content;
+    document.getElementById('display-secondary').textContent = content;
+}
+
+function updateMainDisplay(content = 0) {
+    if(content) content = content.join('');
+    document.getElementById('display-main').textContent = content;
 }
 
 calculator();
